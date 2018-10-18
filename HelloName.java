d33e99ed0f4a741901bf220a2b9e02f63c353716
shuklz <43918340+shuklz@users.noreply.github.com>
2018-10-18 21:53:34 +0800

    Add files via upload

---
+++ HelloName.java
@@ -0,0 +1,14 @@
+// A simple Hello, world program
+// Note: the file name must be the same as the class name,
+// with a .java extension
+// java is case sensitive
+
+package examples;
+
+public class HelloName 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello, Sanjay");
+	}
+}
